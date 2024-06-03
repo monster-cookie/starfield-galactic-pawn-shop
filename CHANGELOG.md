37f17c3a8d9d838ea8afbd7a8c33c893861ef64e
Brian DeGregorio <degregor@blackholeweb.com>
2024-06-03 11:36:51 -0600

    Migrating to the QOG/CK build workflow

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,6 @@
-# Galactic Pawn Shop
+# Venworks Galactic Pawn Shop
 
-## V1.0.13
-* Compatibility with Starfield 1.11.36
-* Ran Starfield .nif Mesh Path Migration Tool for SF 1.11.36 (https://www.nexusmods.com/starfield/mods/9234)
+## Version 2.0.0 [EXPERIMENTAL]
+* Remove RTFP support
+* Convert to Starfield CK for xbox support
+
--- Tools/install.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-REM Notepad++/VSCODE needs current working directory to be where Caprica.exe is 
-cd "C:\Repositories\Public\Starfield Mods\starfield-galactic-pawn-shop\Tools"
-
-del /s /q "D:\MO2Staging\Starfield_Steam\mods\Venworks-GalacticPawnShop-Development\*.ba2"
-copy /y "C:\Repositories\Public\Starfield Mods\starfield-galactic-pawn-shop\Dist\GalacticPawnShop - Main.ba2" "D:\MO2Staging\Starfield_Steam\mods\Venworks-GalacticPawnShop-Development"
